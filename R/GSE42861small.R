7566f7a8c3a751d5151f824a03d22bd33c180709
Fumi <fumihiko@takeuchi.name>
2019-10-12 16:57:10 -0500

    Add data GSE42861small

---
+++ R/GSE42861small.R
@@ -0,0 +1,13 @@
+#' Small Subset of GSE42861 Dataset From GEO
+#'
+#' The dataset includes 336 rheumatoid arthritis cases and 322 controls.
+#' A subset of 500 CpG sites were randomly selected from the original EWAS dataset.
+#'
+#' @docType data
+#'
+#' @usage data(GSE42861small)
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE42861}{GEO}
+#'
+#' @seealso ctassoc
+"GSE42861small"
